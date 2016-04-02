5ea7863bb97a69afa7267c5d38c1431fca91b324
Gopal V <gopalv@apache.org>
2016-04-02 02:34:38 -0400

    slider-gen increase default size

--- slider-gen.sh
+++ slider-gen.sh
@@ -1,2 +1,12 @@
 export JAVA_HOME=/usr/jdk64/jdk1.8.0_65/
-./dist/hive/bin/hive --service llap --instances 1 --cache 32000m --executors 16 --size 170000m --xmx 128000m --loglevel INFO --args "-XX:+UseG1GC -XX:TLABSize=8m -XX:+ResizeTLAB -XX:+UseNUMA -XX:+AggressiveOpts -XX:+AlwaysPreTouch -XX:MetaspaceSize=1024m -XX:InitiatingHeapOccupancyPercent=80 -XX:MaxGCPauseMillis=200 -XX:+PreserveFramePointer" --javaHome $JAVA_HOME
+./dist/hive/bin/hive\
+ --service llap\
+ --instances 1\
+ --cache 48000m\
+ --executors 24\
+ --iothreads 24\
+ --size 180000m\
+ --xmx 128000m\
+ --loglevel WARN\
+ --args "-XX:+UseG1GC -XX:TLABSize=64m -XX:+ResizeTLAB -XX:+UseNUMA -XX:+AggressiveOpts -XX:MetaspaceSize=1024m -XX:InitiatingHeapOccupancyPercent=80 -XX:MaxGCPauseMillis=200 -XX:+PreserveFramePointer"\
+ --javaHome $JAVA_HOME
