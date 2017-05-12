afda2c799817c2defe707d3e4234824fbee67405
Gopal V <gopalv@apache.org>
2017-05-12 02:53:36 -0400

    allow perflogger to be easily enabled

--- hiveserver2.sh
+++ hiveserver2.sh
@@ -1,3 +1,3 @@
 export HADOOP_OPTS="-XX:+UseNUMA -XX:+UseG1GC -Xmx16g" 
-export HADOOP_CLIENT_OPTS="-XX:+UseNUMA -XX:+UseG1GC -Xmx16g" 
+export HADOOP_CLIENT_OPTS="-XX:+UseNUMA -XX:+UseG1GC -Xmx16g -Dhive.perflogger.log.level=WARN" 
 ./dist/hive/bin/hiveserver2 --hiveconf hive.metastore.uris=''
