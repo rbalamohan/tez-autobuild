973ff3b022406be9b076642665400358918dad01
Gopal V <gopalv@apache.org>
2018-02-13 01:55:49 -0500

    fix the libsnappy and log4j issues

--- slider-gen.sh
+++ slider-gen.sh
@@ -5,6 +5,10 @@ if [ "x$JAVA_HOME" = "x" ]; then
    export JAVA_HOME=$(ls -d /usr/lib/jvm/java-1.8.0-openjdk-*/ | head -n 1)
 fi
 
+if [ "x$HADOOP_HOME" = "x" ] && [ test -d /usr/hdp ] ; then
+	export HADOOP_HOME=/usr/hdp/current/hadoop-client
+fi
+
 #export HIVE_AUX_JARS_PATH=$PWD/hive/packaging/target/apache-hive-3.0.0-SNAPSHOT-bin/apache-hive-3.0.0-SNAPSHOT-bin/lib/jetty-rewrite-9.3.8.v20160314.jar
 ./dist/hive/bin/hive\
  --config ./dist/hive/conf/\
